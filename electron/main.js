a57f4cc9802ca4811103890fe713e2b60f57d9e0
Doctorwho14th2024 <anarchymedialibertaire@gmail.com>
2025-01-26 16:14:52 +0100

    fix: correction de la configuration Windows

--- electron/main.js
+++ electron/main.js
@@ -1,31 +1,30 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
-    })
+    });
 
-    win.loadFile('index.html')
+    win.loadURL('https://liberchat-3-0-1.onrender.com/');
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
-})
-
-
+});
